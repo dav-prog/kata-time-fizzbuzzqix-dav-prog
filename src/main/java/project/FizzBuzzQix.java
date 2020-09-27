c2e7a49ed99216b2c1ba37187e539edabf970ac5
David Trimmer <dbtrimmer1@gmail.com>
2020-09-27 14:01:08 -0400

    sevenShouldReturnQix passed

--- src/main/java/project/FizzBuzzQix.java
+++ src/main/java/project/FizzBuzzQix.java
@@ -2,11 +2,14 @@ package project;
 
 public class FizzBuzzQix {
     public String interpret(int numberToInterpret) {
-        if(numberToInterpret % 3 == 0){
+        if (numberToInterpret % 3 == 0) {
             return "Fizz";
         }
-        if(numberToInterpret % 5 == 0){
+        if (numberToInterpret % 5 == 0) {
             return "Buzz";
+        }
+        if (numberToInterpret % 7 == 0) {
+            return "Qix";
         }
 
         return "" + numberToInterpret;
--- src/test/java/project/FizzBuzzQixTest.java
+++ src/test/java/project/FizzBuzzQixTest.java
@@ -5,21 +5,30 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzQixTest {
-   private FizzBuzzQix underTest = new FizzBuzzQix();
+    private FizzBuzzQix underTest = new FizzBuzzQix();
+
     @Test
     public void oneShouldReturnOne() {
         String spokenWord = underTest.interpret(1);
         assertThat(spokenWord).isEqualTo("1");
     }
+
     @Test
-    public void threeShouldReturnFizz(){
+    public void threeShouldReturnFizz() {
         String spokenWord = underTest.interpret(3);
         assertThat(spokenWord).isEqualTo("Fizz");
     }
+
     @Test
-    public void fiveShouldReturnBuzz(){
+    public void fiveShouldReturnBuzz() {
         String spokenWord = underTest.interpret(5);
         assertThat(spokenWord).isEqualTo("Buzz");
+    }
+
+    @Test
+    public void sevenShouldReturnQix() {
+        String spokenWord = underTest.interpret(7);
+        assertThat(spokenWord).isEqualTo("Qix");
     }
 
 }
